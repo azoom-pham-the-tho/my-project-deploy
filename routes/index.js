a76deb6f0c8f18cfdc70362e52fd7872c877e8ae
Phạm Thế Tho <pham.the.tho@azoom.jp>
2023-09-26 02:49:31 +0000

    update-cloud-builder

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,8 @@ router.get("/", function (req, res) {
 });
 
 router.get("/health", function (req, res) {
-  if (req.connect) return res.status(200).json({ message: "connect ok" });
-  res.status(400).json({ message: "connect error" });
+  if (req.connect) return res.status(200).json({ message: "connect ok", env: process.env });
+  res.status(400).json({ message: "connect error", env: process.env });
 });
 
 router.get("/secret", function (req, res) {
